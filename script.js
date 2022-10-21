035bca9e0e4fefcbb49e6f45e6df6d3b9451e923
dutra-86 <gabrielsprecherdutra@gmail.com>
2022-10-21 19:19:51 -0300

    Implementada lógica de adição de cartas via prompt e mecanismo virar carta

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function virar(card){
+    card.classList.toggle("virada");
+}
+
+
 qtd_cartas = 0;
 while(qtd_cartas < 4 || qtd_cartas > 14 || qtd_cartas%2==1){
     qtd_cartas = prompt("Insira a quantidade de cartas (número par entre 4 e 14)");
@@ -11,4 +16,31 @@ for (let i = 0; i < qtd_cartas/2; i++){
     baralho.push(allcards[i]);
 }
 
+//EMBARALHARRRRR
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+baralho.sort(comparador);
+
 console.log(baralho);
+
+for (let i = 0; i < qtd_cartas/2; i++){
+    document.querySelector(".primeira").innerHTML +=
+    `<div class="carta ${baralho[i]}" onclick="virar(this)">
+    <div class="back face">
+    <img src="media/back.png"/>
+    </div>
+    <div class="front face">
+    <img src="media/${baralho[i]}parrot.gif"/>
+    </div>
+    </div>`;
+    document.querySelector(".segunda").innerHTML +=
+    `<div class="carta ${baralho[(qtd_cartas/2)+i]}" onclick="virar(this)">
+    <div class="back face">
+    <img src="media/back.png"/>
+    </div>
+    <div class="front face">
+    <img src="media/${baralho[(qtd_cartas/2)+i]}parrot.gif"/>
+    </div>
+    </div>`;
+}
