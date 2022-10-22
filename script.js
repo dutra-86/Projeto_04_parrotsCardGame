ffbf4344d06b03d243aeb14ab8c204e7e64f7714
dutra-86 <gabrielsprecherdutra@gmail.com>
2022-10-21 21:41:48 -0300

    Implementada a lógica do jogo

--- script.js
+++ script.js
@@ -1,46 +1,96 @@
-function virar(card){
-    card.classList.toggle("virada");
+function venceu(){
+    document.querySelector(".primeira").innerHTML = '';
+    document.querySelector(".segunda").innerHTML = '';
+    alert("Parabéns, você venceu!");
+    let again = '';
+    while (again != 'sim'){
+        again = prompt("Deseja jogar novamente? (sim/nao)");
+        if (again == 'nao'){
+            break;
+        }
+    }
+    if (again == 'sim'){
+        game();
+    }
 }
 
-
-qtd_cartas = 0;
-while(qtd_cartas < 4 || qtd_cartas > 14 || qtd_cartas%2==1){
-    qtd_cartas = prompt("Insira a quantidade de cartas (número par entre 4 e 14)");
+function desvirar(c1,c2){
+    document.querySelectorAll(`.${c1}`)[0].classList.remove("virada");
+    document.querySelectorAll(`.${c1}`)[1].classList.remove("virada");
+    document.querySelectorAll(`.${c2}`)[0].classList.remove("virada");
+    document.querySelectorAll(`.${c2}`)[1].classList.remove("virada");
 }
 
-allcards = ['bobross', 'explody', 'fiesta', 'metal', 'revertit', 'triplets', 'unicorn'];
-baralho = [];
-
-for (let i = 0; i < qtd_cartas/2; i++){
-    baralho.push(allcards[i]);
-    baralho.push(allcards[i]);
+function virar(card, tipo){
+    if (card.classList.contains("virada") == false){
+        card.classList.toggle("virada");
+        cartas_selecionadas.push(tipo);
+        if (cartas_selecionadas.length == 2){
+            if (cartas_selecionadas[0] != cartas_selecionadas[1]){
+                setTimeout(desvirar,1000,cartas_selecionadas[0],cartas_selecionadas[1]);
+            }
+            else{
+                //ACHOU O PAR
+                contador_acertos ++;
+                if (contador_acertos == qtd_cartas/2){
+                    setTimeout(venceu,1000);
+                }
+            }
+            cartas_selecionadas = [];
+        }
+    }
 }
 
-//EMBARALHARRRRR
-function comparador() { 
-	return Math.random() - 0.5; 
-}
-baralho.sort(comparador);
+let contador_acertos = 0;
+let cartas_selecionadas=[];
+let qtd_cartas = 0;
 
-console.log(baralho);
+function game(){
+    qtd_cartas = 0;
+    contador_acertos = 0;
+    cartas_selecionadas=[];
 
-for (let i = 0; i < qtd_cartas/2; i++){
-    document.querySelector(".primeira").innerHTML +=
-    `<div class="carta ${baralho[i]}" onclick="virar(this)">
-    <div class="back face">
-    <img src="media/back.png"/>
-    </div>
-    <div class="front face">
-    <img src="media/${baralho[i]}parrot.gif"/>
-    </div>
-    </div>`;
-    document.querySelector(".segunda").innerHTML +=
-    `<div class="carta ${baralho[(qtd_cartas/2)+i]}" onclick="virar(this)">
-    <div class="back face">
-    <img src="media/back.png"/>
-    </div>
-    <div class="front face">
-    <img src="media/${baralho[(qtd_cartas/2)+i]}parrot.gif"/>
-    </div>
-    </div>`;
+    while(qtd_cartas < 4 || qtd_cartas > 14 || qtd_cartas%2==1){
+        qtd_cartas = prompt("Insira a quantidade de cartas (número par entre 4 e 14)");
+    }
+
+    allcards = ['bobross', 'explody', 'fiesta', 'metal', 'revertit', 'triplets', 'unicorn'];
+    baralho = [];
+
+    for (let i = 0; i < qtd_cartas/2; i++){
+        baralho.push(allcards[i]);
+        baralho.push(allcards[i]);
+    }
+
+    //EMBARALHARRRRR
+    function comparador() { 
+        return Math.random() - 0.5; 
+    }
+    baralho.sort(comparador);
+
+    console.log(baralho);
+
+    for (let i = 0; i < qtd_cartas/2; i++){
+        document.querySelector(".primeira").innerHTML +=
+        `<div class="carta ${baralho[i]}" onclick="virar(this,'${baralho[i]}')">
+        <div class="back face">
+        <img src="media/back.png"/>
+        </div>
+        <div class="front face">
+        <img src="media/${baralho[i]}parrot.gif"/>
+        </div>
+        </div>`;
+        document.querySelector(".segunda").innerHTML +=
+        `<div class="carta ${baralho[(qtd_cartas/2)+i]}" onclick="virar(this,'${baralho[(qtd_cartas/2)+i]}')">
+        <div class="back face">
+        <img src="media/back.png"/>
+        </div>
+        <div class="front face">
+        <img src="media/${baralho[(qtd_cartas/2)+i]}parrot.gif"/>
+        </div>
+        </div>`;
+    }
+
 }
+
+game();
